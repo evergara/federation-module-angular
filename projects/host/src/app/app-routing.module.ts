80bd2b3acfb044a210e4d72c73f04d0c6bc2f188
Emerson Vergara <ee-web@hotmail.com>
2022-03-06 20:47:48 -0500

    configuracion de host

--- projects/host/src/app/app-routing.module.ts
+++ projects/host/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
-import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+
 const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'}
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {
+    path: 'mf',
+    loadChildren: () =>
+      import('mf/ShowinfoModule').then((m) => m.ShowinfoModule),
+  }
 ];
 
 @NgModule({
--- projects/host/webpack.config.js
+++ projects/host/webpack.config.js
@@ -28,25 +28,23 @@ module.exports = {
     new ModuleFederationPlugin({
         library: { type: "module" },
 
-        // For remotes (please adjust)
-        // name: "host",
-        // filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './projects/host/src/app/app.component.ts',
-        // },        
-        
+    
         // For hosts (please adjust)
-        // remotes: {
-        //     "mfTodo": "http://localhost:4200/remoteEntry.js",
-
-        // },
+        remotes: {
+          mfTodo : "http://localhost:4200/remoteEntry.js",
+        },
 
         shared: share({
           "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
+          rxjs: {
+            singleton: true,
+            strictVersion: true,
+            requiredVersion: 'auto',
+            includeSecondaries: true,
+          },
           ...sharedMappings.getDescriptors()
         })
         
